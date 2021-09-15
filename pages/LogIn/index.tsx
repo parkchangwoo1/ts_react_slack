211680f88ac5c51cc25f2c89f22e47a481fa1ad5
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-09-15 14:40:29 +0900

    add: LogIn, SignUp (초기 파일)

---
+++ pages/LogIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogIn = () => {
+  return (
+    <div>
+      <div>로그인</div>
+    </div>
+  );
+};
+
+export default LogIn;
---
+++ pages/SignUp/index.tsx
@@ -0,0 +1,84 @@
+import React, { useCallback, useState } from 'react';
+import { Form, Label, Input, Button, LinkContainer, Header, Error } from './styles';
+import useInput from '@hooks/useinput';
+
+const SignUp = () => {
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [mismatchError, setMissmatchError] = useState(false);
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMissmatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck],
+  );
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMissmatchError(e.target.value !== password);
+    },
+    [password],
+  );
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!mismatchError) {
+        console.log('회원가입');
+      }
+    },
+    [email, nickname, password, passwordCheck],
+  );
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {/* {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        {/* <Link to="/login">로그인 하러가기</Link> */}
+      </LinkContainer>
+    </div>
+  );
+};
+
+export default SignUp;
